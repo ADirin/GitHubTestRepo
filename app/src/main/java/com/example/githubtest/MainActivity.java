2e95dde5b93a46971c2f55ae57e701cb812c2b03
Amir Dirin <amir.dirin@yahoo.com>
2022-11-27 22:12:32 +0200

    Added button functionality

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button btnClickMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        textView = findViewById(R.id.txtName);
+        btnClickMe = findViewById(R.id.btnClick);
+
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello my name is AMir");
+            }
+        });
+
+
+;    }
 }
